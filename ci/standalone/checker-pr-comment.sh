db36a461f891949208f7d0217b8fd31ca093a47b
Geunsik Lim <geunsik.lim@samsung.com>
2018-07-19 16:16:41 +0900

    Added version string for maintenance
    This commit is to display version string information as well as PR writing guide
    to handle consistently lots of different CI versions when new PR is created.
    
    **Changes proposed in this PR:**
    1. Added display of version string for maintenance
    2. Updated guide message (Just once) for a PR
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- ci/standalone/checker-pr-comment.sh
+++ ci/standalone/checker-pr-comment.sh
@@ -40,7 +40,7 @@ input_pr=$1
 
 if [[ $pr_comment_notice == 1 ]]; then
     # inform PR submitter of a rule to pass the CI process
-    message="Thank you for submitting PR #${input_pr}. Your PR has to pass all verificiation processes of cibot before getting a review process from reviewers. We recommend that you read instruction manual in `Documentation folder`. If you want to monitor the progress status of your PR in detail, visit ${CISERVER}."
+    message=":memo: Version: ${VERSION}. Thank you for submitting PR #${input_pr}. Your PR must pass all verificiation processes of cibot before starting a review process from reviewers. If you are new member to join this project, please read manuals in documentation folder and wiki page. In order to monitor a progress status of your PR in more detail, visit ${CISERVER}."
     cibot_comment $TOKEN "$message" "$GITHUB_WEBHOOK_API/issues/$input_pr/comments"
 fi
 
