c3e2eb263eb0c9e6d509456e3c26c6b57b76788a
sewon.oh <sewon.oh@samsung.com>
2018-09-07 16:16:00 +0900

    [coverage] Remove password request
    Add sudo command to remove password request
    and fix error and typo
    
    Signed-off-by: sewon.oh <sewon.oh@samsung.com>

--- ci/gcov/coverage-generator.sh
+++ ci/gcov/coverage-generator.sh
@@ -38,19 +38,23 @@ check_dependency rpm2cpio
 check_dependency cpio
 
 # Build a RPM file to geneate a code coverage statistics with gcov/lcov.
+# gbs command needs 'sudo' command to get 'root' privilege
 gbs build -A x86_64 --define "testcoverage 1" -B $dirpath $build_root --clean
 
 # Extract rpm to gcov_html folder
 cp $dirpath/local/repos/tizen/x86_64/RPMS/nnstreamer-unittest-coverage* $dirpath
 pushd $dirpath
 rpm2cpio $dirpath/nnstreamer-unittest-coverage* | cpio -idvm 
+if [[ -d ../gcov/html ]]; then
+    rm -rf ../gcov_html
+fi
 mkdir -p ../gcov_html
 mv -f usr/share/nnstreamer/unittest/result/* ../gcov_html
 popd
 
 # Remove unused folder. 
 # Note that we have to use 'sudo' command  to remove these files 
-# because 'gbs' command creates some files with "root' ID 
+# because 'gbs' command creates some files with 'root' ID 
 # via "sudo chroot" operation.
 sudo rm -rf $dirpath/local
 sudo rm -rf $dirpath/usr
