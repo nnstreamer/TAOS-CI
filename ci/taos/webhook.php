a10e4d084dea70ebd03302eccb54c714198a27cd
sewon.oh <sewon.oh@samsung.com>
2018-10-02 15:40:15 +0900

    [typo] missng ')'
    It is trivial. just add ')'
    
    Signed-off-by: sewon.oh <sewon.oh@samsung.com>

--- ci/taos/webhook.php
+++ ci/taos/webhook.php
@@ -71,7 +71,7 @@ function json_config(){
     // Note that you have to use 'print_r' instead of echo function to display decoded json data.
     // print_r ($json_config);
     printf ("[DEBUG] json config: your webhook ID: %s\n<br>", $json_config->github->id);
-    printf ("[DEBUG] json config: your webhook secret: %s\n<br>", substr_replace("$json_config->github->secret","******",3);
+    printf ("[DEBUG] json config: your webhook secret: %s\n<br>", substr_replace("$json_config->github->secret","******",3));
 
     // Please, add "$hookSecret" value in Secret field at https://<github-address>/.../setting/hooks/
     // Set NULL if you want to disable this security.
