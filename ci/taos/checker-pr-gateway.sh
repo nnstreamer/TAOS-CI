1e9e13ff6aae50e13bdceea511c6727c01d920ac
Geunsik Lim <leemgs@gmail.com>
2018-10-17 08:22:34 +0900

    PR-gateway: run git command just once
    This commit is to execute 'git clone' command just once when
    contributor submits a PR. This commit is a final step to complete
    the issue #299.
    
    * Fixed issue #299.
    
    Signed-off-by: Geunsik Lim <leemgs@gmail.com>

--- ci/taos/checker-pr-gateway.sh
+++ ci/taos/checker-pr-gateway.sh
@@ -88,6 +88,50 @@ fi
 cd $dir_ci
 mkdir -p $dir_commit
 
+# --------------------------- git-clone module: clone a git repository ----------------------
+echo "[DEBUG] Starting pr-audit....\n"
+
+# check if existing folder already exists
+if [[ -d $dir_commit ]]; then
+    echo "[DEBUG] WARN: mkdir command is failed because $dir_commit directory already exists."
+else
+    echo "[DEBUG] WARN: mkdir command is failed because $dir_commit directory does not exists."
+fi
+
+# check if github project folder already exists
+pwd
+cd $dir_commit
+if [[ -d ${PRJ_REPO_OWNER} ]]; then
+    echo "[DEBUG] WARN: ${PRJ_REPO_OWNER} already exists and is not an empty directory."
+    echo "[DEBUG] WARN: Removing the existing directory..."
+    rm -rf ./${PRJ_REPO_OWNER}
+fi
+
+# create 'report' folder to archive log files.
+mkdir ./report
+
+# run "git clone" command to download git source
+# options of 'sudo' command: 
+# 1) The -H (HOME) option sets the HOME environment variable to the home directory of the target user (root by default)
+# as specified in passwd. By default, sudo does not modify HOME.
+# 2) The -u (user) option causes sudo to run the specified command as a user other than root. To specify a uid instead of a username, use #uid.
+pwd
+sudo -Hu www-data git clone --reference ${REFERENCE_REPOSITORY} $input_repo
+if [[ $? != 0 ]]; then
+    echo "[DEBUG] ERROR: 'git clone' command is failed because of incorrect setting of CI server."
+    echo "[DEBUG] Please check /var/www/ permission, /var/www/html/.netrc, and /var/www/html/.gbs.conf."
+    echo "[DEBUG] current id: $(id)"
+    echo "[DEBUG] current path: $(pwd)"
+    echo "[DEBUG] $ sudo -Hu www-data git clone --reference ${REFERENCE_REPOSITORY} $input_repo"
+    exit 1
+fi
+
+# run "git branch" to use commits from PR branch
+cd ./${PRJ_REPO_OWNER}
+git checkout -b $input_branch origin/$input_branch
+git branch
+
+
 # --------------------------- Run a checker: format ---------------------------------------
 cd $dir_ci
 echo -e "[DEBUG] dir_commit is $dir_commit. This folder is created." | tee    $log_file_format
