66cdaa219feac0b83ec5c49d9f7e6c1d31028532
Geunsik Lim <geunsik.lim@samsung.com>
2018-11-14 16:50:27 +0900

    Webapp: Support "broken arrow" button to handle PR hangs
    This commit is to provide "broken arrow" button to handle unexpected system
    situation such as PR hangs, frequent OOM operations, and too many zombie
    processes.
    
    **Changes proposed in this PR:**
    
    * Version 6:
    1. Updated variables (e.g., id, ip, password) for code readability
    
    * Version 5:
    1. Added a security facility to contol IP address
       - Check if IP address of a system administrator
         is equal to an allowed IP address.
    
    * Version 4:
    1. Updated a html design of Broken arrow webapge.
    
    * Version 3:
    1. Improved try ... catch statement for readability
    2. Fixed incorrect file name : broke_arrow_proceed.php
    3. Added comments for maintenance
    
    * Version 2:
    1. Added configuration variables in json file
    2. Added a routine to handle POST html tag
    3. Updated check routine if id and passowr are correct
    4. Added doxygtags
    5. Specified php files with permission 644
    
    * Version 1:
    1. Supported "broken arrow" button
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

---
+++ ci/taos/webapp/broken_arrow.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Broken Arrow</title>
+</head>
+<body>
+<font size=18>"Broken Arrow" Situation: </font> <br>
+<br><br>
+This interface is to run the "<b>Broken Arrow</b>" strategy when the system administrator <br>
+can not find a recipe to solve a cirtical issue such as PR hangs due to unknown reasons.<br>
+
+<ul> <li>
+Broken Arrow refers to an accidental event that involves nuclear weapons, warheads<br>
+or components which does not create the risk of nuclear war. These include unexplained<br>
+nuclear detonation, non-nuclear detonation, radioactive contamination, and public hazard.
+</li>
+</ul>
+<br><br>
+
+<form action="broken_arrow_proceed.php" method="post">
+<table border=0>
+<tr>
+<td><img src=./circle.png with=20 height=20>ID</td><td><input type="text" name="id"><br></td>
+</tr>
+<tr>
+<td><img src=./circle.png with=20 height=20>Password</td><td><input type="password" name="pass"><br></td>
+</tr>
+<tr>
+<td>
+<input type="submit"></td>
+<td></td>
+</tr>
+</form>
+
+</body>
+</html>
+
