d1d0a386eefbbbd7f50105f4b1678af0e54bdb25
Geunsik Lim <geunsik.lim@samsung.com>
2019-02-27 13:48:00 +0900

    Maintenance: Update the existing comment of an issue checker
    It's trivial. This commmit is to update the existing annotation to improve
    a readability and maintenance of the issue check module.
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- ci/taos/checker-issue-comment.sh
+++ ci/taos/checker-issue-comment.sh
@@ -15,10 +15,10 @@
 #
 
 ##
-# @file  checker-issue-comment.sh
-# @brief issue facility to comment automatically whenever issue happens .
-# @see		https://github.com/nnsuite/TAOS-CI
-# @author	Geunsik Lim <geunsik.lim@samsung.com>
+# @file   checker-issue-comment.sh
+# @brief  An issue facility to comment automatically whenever issue happens .
+# @see    https://github.com/nnsuite/TAOS-CI
+# @author Geunsik Lim <geunsik.lim@samsung.com>
 # @param
 #  arg1: issue number
 #
@@ -27,16 +27,16 @@
 source ./config/config-environment.sh
 source ./common/api_collection.sh
 
-# check if input argument is correct. 
+# Check if an input argument is correct.
 if [[ $1 == "" ]]; then
-    printf "[DEBUG] ERROR: Please, use a correct issue number.\n"
+    printf "[DEBUG] ERROR: Please use a correct issue number.\n"
     exit 1
 fi
 
 input_issue=$1
 
-# --------------------------- Report module: submit check result to github-website --------------
-# execute automatic comment on new issue.
+# --------------------------- Report module: submit a check result to a github website --------------
+# Submit an appropriate reply automatically whenever new issue is created.
 
 message=":octocat: **cibot**: Thank you for posting issue #$input_issue. The person in charge will reply soon."
 cibot_comment $TOKEN "$message" "$GITHUB_WEBHOOK_API/issues/$input_issue/comments"
