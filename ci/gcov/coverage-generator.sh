be42f874c1a65161a458761f6f5e3d6d77b7fff0
Geunsik Lim <geunsik.lim@samsung.com>
2019-03-26 18:35:41 +0900

    Code Coverage: Improved the coverage generator
    Fixed issue #https://github.com/nnsuite/nnstreamer/issues/1275.
    
    This commit is to improve the exsiting coverage generator
    to fix an unexpected situation that we sometimes have got a
    code coverage report based on out-of-date version.
    
    **Changelog**
    1. Rewrote the operation flow of coverage generator
    2. Keep the bash writing style with a standard grammar
       to support a compatibility  between different bash versions
    3. Added debug message to figure out a reason in case of an error
    4. Improved gbs build command to avoid incorrect reference of a
       repository cache folder.
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- ci/gcov/coverage-generator.sh
+++ ci/gcov/coverage-generator.sh
@@ -15,48 +15,67 @@
 #
 
 ##
-# @file coverage-generator.sh
-# @brief Auto-generate unit test coverage files
-# @how to append this script to /etc/crontab
-#       $ sudo vi /etc/crontab
-#       30 * * * * www-data /var/www/html/nnstreamer/ci/gcov/coverage-generator.sh
-# @see      https://github.com/nnsuite/TAOS-CI
+# @file     coverage-generator.sh
+# @brief    Auto-generate unit test coverage files
 # @author   Sewon Oh <sewon.oh@samsung.com>
+# @author   Geunsik Lim <geunsik.lim@samsung.com>
+# @note
+#  How to append this script to /etc/crontab
+#  $ sudo vi /etc/crontab
+#  30 * * * * www-data /var/www/html/nnstreamer/ci/gcov/coverage-generator.sh
+# @see      https://github.com/nnsuite/TAOS-CI
 # @param    None
-#
 
-# Set-up environements
-dirpath="$( cd "$( dirname "$0")" && pwd )"
-build_root="${dirpath}/../../../"
+# Calculate the gcov and git directory from this file that include a absolute path
+gcov_dir="$( cd "$( dirname "$0")" && pwd )"
+cd ../../
+git_dir="$(pwd)"
+cd -
+echo -e "[DEBUG] gcov_dir is '$gcov_dir'"
+echo -e "[DEBUG] git_dir is '$git_dir'"
 
-# Include comman api
-source $dirpath/../taos/common/api_collection.sh
+# Include the comman api
+source $git_dir/ci/taos/common/api_collection.sh
 
 # Check dependency
 check_dependency gbs
 check_dependency rpm2cpio
 check_dependency cpio
 
-# Build a RPM file to geneate a code coverage statistics with gcov/lcov.
-# gbs command needs 'sudo' command to get 'root' privilege
-gbs build -A x86_64 --define "testcoverage 1" --define "unit_test 1" -B $dirpath $build_root --clean
+pushd $git_dir
+# Generate a code coverage statistics with gcov/lcov.
+# Note that the gbs command requires a 'sudo' command to get a 'root' privilege
+echo -e "[DEBUG] The current folder is '$(pwd)'."
+echo -e "[DEBUG] Running 'gbs' command to generate a rpm file."
+gbs build -A x86_64 --clean --define "testcoverage 1" --define "unit_test 1" -B $gcov_dir
+result=$?
+if [[ $result != 0 ]]; then
+    echo -e "[DEBUG] Ooops. 'gbs build' command is failed."
+    echo -e "[DEBUG] Stopping this task..."
+    exit 1
+fi
 
-# Extract rpm to gcov_html folder
-cp $dirpath/local/repos/tizen/x86_64/RPMS/nnstreamer-unittest-coverage* $dirpath
-pushd $dirpath
-rpm2cpio $dirpath/nnstreamer-unittest-coverage* | cpio -idvm 
-rm $dirpath/nnstreamer-unittest-coverage*
+# Decompress the rpm file to  a gcov_html folder
+cd $gcov_dir/
+echo -e "[DEBUG] Copying generated rpm files to $gcov_dir folder."
+cp $gcov_dir/local/repos/tizen/x86_64/RPMS/nnstreamer-unittest-coverage* $gcov_dir
+echo -e "[DEBUG] Decompressing the rpm files in $gcov_dir folder."
+rpm2cpio $gcov_dir/nnstreamer-unittest-coverage* | cpio -idumv
+echo -e "[DEBUG] Removing the rpm files in $gcov_dir folder."
 if [[ -d ../gcov_html ]]; then
     rm -rf ../gcov_html
 fi
 mkdir -p ../gcov_html
 mv -f usr/share/nnstreamer/unittest/result/* ../gcov_html
-popd
+cd -
 
 # Remove unused folder. 
 # Note that we have to use 'sudo' command  to remove these files 
-# because 'gbs' command creates some files with 'root' ID 
-# via "sudo chroot" operation.
-sudo rm -rf $dirpath/local
-sudo rm -rf $dirpath/usr
+# because 'gbs' command creates some files with "sudo chroot" operation.
+sudo rm -rf $gcov_dir/nnstreamer-unittest-coverage*
+sudo rm -rf $gcov_dir/local
+sudo rm -rf $gcov_dir/usr
 
+echo -e "[DEBUG] The current folder is '$(pwd)'."
+echo -e "[DEBUG] The test coverage is executed."
+popd
