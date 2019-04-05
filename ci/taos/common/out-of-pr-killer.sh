f27e50a68d9fbc089de3688be11c406d3d9b8ac4
Geunsik Lim <geunsik.lim@samsung.com>
2019-04-05 11:19:11 +0900

    doc: updated the comment of OOP killer
    This commit is trivial. It is to update the existing comment of OOP killer
    for maintenance and readability.
    
    Signed-off-by: Geunsik Lim <geunsik.lim@samsung.com>

--- ci/taos/common/out-of-pr-killer.sh
+++ ci/taos/common/out-of-pr-killer.sh
@@ -16,18 +16,19 @@
 
 ##
 # @file   out-of-pr-killer.sh
-# @brief  Out-of-PR((OOP) Killer
+# @auther Geunsik Lim <geunsik.lim@samsung.com>
+# @brief  Out-of-PR(OOP) Killer
 #
-# This facility is to stop compulsorily the previous same PRs invoked by
-# checker-pr-gateway.sh when the developers try to send a lot of same PRs
-# repeatedly.
+# This module runs when there is no available queue to run a PR.
+# It is to kill compulsorily the previous same PRs invoked by
+# checker-pr-gateway.sh when the developers resend same PRs repeatedly.
 # 
 
 
-# --------------------------- Out-of-PR (OOP) killer: kill previous same PRs  ----------------------------------
+# --------------------------- Out-of-PR (OOP) killer: kill previous duplicated PRs  ----------------------------------
 
 ##
-# @brief Run out-of-pr(OOP) killer
+# @brief Run Out-of-PR(OOP) killer
 function run_oop_killer(){
     # Specify dependent commands for this function.
     check_dependency ps
@@ -48,21 +49,22 @@ function run_oop_killer(){
         # Step 1: The victim pid1 is checker-pr-gateway.sh (It is a task distributor.)
         # Step 2: The victim pid2 is checker-pr-audit-async.sh (It is a audit group.)
         # Step 3:
-        # a. The victim pid3_tizen:  "gbs build" command for a tizen build
-        # b. The victim pid3_ubuntu: "pdebuild" command for a ubuntu build 
+        # a. The victim pid3_tizen:  "gbs build" command for a Tizen build
+        # b. The victim pid3_ubuntu: "pdebuild" command for a Ubuntu build
         victim_pid1=`ps -ef | grep bash | grep checker-pr-gateway.sh       | grep $input_pr | grep $victim_date | awk '{print $2}'`
         victim_pid2=`ps -ef | grep bash | grep checker-pr-audit-async.sh | grep $input_pr | grep $victim_date | awk '{print $2}'`
         victim_pid3_tizen=`ps -ef | grep python | grep gbs | grep "_pr_number $input_pr" | grep $victim_date | awk '{print $2}'`
         victim_pid3_ubuntu=`ps -ef | grep bash | grep pdebuild | grep "_pr_number $input_pr" | grep $victim_date | awk '{print $2}'`
-        # Todo: NYI, Implement the OOP killer for Yocto  build (devtool)
+        # Todo: NYI, Implement the OOP killer for Yocto build (devtool)
         victim_pid3_yocto=""
+        victim_pid3_android=""
     
-        # The process killer allows to kill only task(s) in case that there are running lots of tasks with same PR number.
+        # The OOP killer destroy the examination of out-of-date PRs when developers have resbumitted a PR repeatedly.
         if [[ ("$victim_pr" -eq "$input_pr") && (1 -eq "$(echo "$victim_date < $input_date" | bc)") ]]; then
-            echo "[DEBUG] OOP Killer: Killing 'checker-pr-gateway.sh' process..."
+            echo "[DEBUG] OOP Killer: Killing 'checker-pr-gateway.sh' process ($victim_pid1) ..."
             kill $victim_pid1
 
-            echo "[DEBUG] OOP Killer: Killing 'checker-pr-audit-async.sh' process..."
+            echo "[DEBUG] OOP Killer: Killing 'checker-pr-audit-async.sh' process ($victim_pid2) ..."
             kill $victim_pid2
 
             echo "[DEBUG] OOP Killer/Tizen: victim_pr=$victim_pr, input_pr=$input_pr, victim_date=$victim_date, input_date=$input_date "
@@ -73,7 +75,7 @@ function run_oop_killer(){
             echo "[DEBUG] OOP Killer/Ubuntu: killing PR $victim_pr (pid <$victim_pid1> <$victim_pid2> <$victim_pid3_ubuntu>)."
             kill $victim_pid3_ubuntu
 
-            # Handle a possibility that someone updates a single PR multiple times within 1 second.
+            # Sleep for 1 second to handle a situation that someone updates a PR repeatedly.
             sleep 1
             echo "[DEBUG] OOP Killer: removing the out-of-date ./${dir_worker}/${victim_pr}-${victim_date}-* folder"
             rm -rf ./${dir_worker}/${victim_pr}-${victim_date}-*
