4f16e0d0aa25b48ad986ab7ba5225da2168cc9b0
Jihoon Lee <jhoon.it.lee@samsung.com>
2021-04-22 11:42:52 +0900

    [Doxygen Tag] Add report for the plugin
    This patch enables to generate a report for the doxygen tag plugin
    
    Signed-off-by: Jihoon Lee <jhoon.it.lee@samsung.com>

--- ci/taos/plugins-good/pr-prebuild-doxygen-tag.sh
+++ ci/taos/plugins-good/pr-prebuild-doxygen-tag.sh
@@ -29,6 +29,8 @@
 ##
 # @brief [MODULE] ${BOT_NAME}/pr-prebuild-doxygen-tag
 function pr-prebuild-doxygen-tag(){
+    report_path=../report/doxygen_tag.txt
+
     echo "########################################################################################"
     echo "[MODULE] ${BOT_NAME}/pr-prebuild-doxygen-tag: Check if source code includes required Doxygen tags for Doxygen documentation."
     # Inspect all *.patch files that are fetched from a commit file
@@ -38,18 +40,18 @@ function pr-prebuild-doxygen-tag(){
     for curr_file in ${FILELIST}; do
         # if a current file is located in $SKIP_CI_PATHS_FORMAT folder, let's skip the inspection process
         if [[ "$curr_file" =~ ($SKIP_CI_PATHS_FORMAT)$ ]]; then
-            echo "[DEBUG] Doxygen checker skips the Doxygen inspection because $curr_file is located in the \"$SKIP_CI_PATHS_FORMAT\"."
+            echo "[DEBUG] Doxygen checker skips the Doxygen inspection because $curr_file is located in the \"$SKIP_CI_PATHS_FORMAT\"." >> $report_path
             continue
         fi
 
-        echo "[DEBUG] The current file name is (${curr_file}). "
+        echo "[DEBUG] The current file name is (${curr_file}). " >> $report_path
         # Handle only text files in case that there are lots of files in one commit.
         if [[ `file $curr_file | grep "ASCII text" | wc -l` -gt 0 ]]; then
             # In case of source code files: *.c|*.h|*.cpp|*.py|*.sh|*.php )
             case $curr_file in
                 # In case of C/C++ code
                 *.c|*.h|*.cc|*.hh|*.cpp|*.hpp )
-                    echo "[DEBUG] ( $curr_file ) file is source code with the text format."
+                    echo "[DEBUG] ( $curr_file ) file is source code with the text format." >> $report_path
                     doxygen_lang="doxygen-cncpp"
                     # Append a doxgen rule step by step
                     doxygen_basic_rules="@file @brief" # @file and @brief to inspect file
@@ -68,14 +70,14 @@ function pr-prebuild-doxygen-tag(){
 
                     for word in $doxygen_basic_rules
                     do
-                        # echo "[DEBUG] $doxygen_lang: doxygen tag for current $doxygen_lang code is $word."
+                        # echo "[DEBUG] $doxygen_lang: doxygen tag for current $doxygen_lang code is $word." >> $report_path
                         doxygen_rule_compare_count=`cat ${curr_file} | grep "$word" | wc -l`
                         doxygen_rule_expect_count=1
 
                         # Doxygen_rule_compare_count: real number of Doxygen tags in a file
                         # Doxygen_rule_expect_count: required number of Doxygen tags
                         if [[ $doxygen_rule_compare_count -lt $doxygen_rule_expect_count ]]; then
-                            echo "[ERROR] $doxygen_lang: failed. file name: $curr_file, $word tag is required at the top of file"
+                            echo "[ERROR] $doxygen_lang: failed. file name: $curr_file, $word tag is required at the top of file" >> $report_path
                             check_result="failure"
                             global_check_result="failure"
                             latest_failed_file=$curr_file
@@ -97,13 +99,13 @@ function pr-prebuild-doxygen-tag(){
 
                         # Find line number of functions using ctags, and append them.
                         while IFS='' read -r line || [[ -n "$line" ]]; do
-                            temp=`echo $line | cut -d ' ' -f3` # line number of function place 3rd field when divided into ' '
+                            temp=`echo $line | cut -d ' ' -f3` # line number of function place 3rd field when divided into ' ' >> $report_path
                             function_positions="$function_positions $temp "
                         done < <(ctags -x --c-kinds=$function_check_flag $curr_file) # "--c-kinds=f" mean find function
 
                         # Find line number of structure using ctags, and append them.
                         while IFS='' read -r line || [[ -n "$line" ]]; do
-                            temp=`echo $line | cut -d ' ' -f3` # line number of structure place 3rd field when divided into ' '
+                            temp=`echo $line | cut -d ' ' -f3` # line number of structure place 3rd field when divided into ' ' >> $report_path
                             structure_positions="$structure_positions $temp "
                         done < <(ctags -x --c-kinds=sc $curr_file) # "--c-kinds=sc" mean find 's'truct and 'c'lass
 
@@ -115,7 +117,7 @@ function pr-prebuild-doxygen-tag(){
                             # To pass correct line number not sub number, keep space " $idx ".
                             # ex) want to pass 143 not 14, 43, 1, 3, 4
                             if [[ $function_positions =~ " $idx " && $brief -eq 0 ]]; then
-                                echo "[ERROR] File name: $curr_file, $idx line, `echo $line | cut -d ' ' -f1` function needs @brief tag "
+                                echo "[ERROR] File name: $curr_file, $idx line, `echo $line | cut -d ' ' -f1` function needs @brief tag " >> $report_path
                                 check_result="failure"
                                 global_check_result="failure"
                                 latest_failed_file=$curr_file
@@ -125,7 +127,7 @@ function pr-prebuild-doxygen-tag(){
                             # To pass correct line number not sub number, keep space " $idx ".
                             # For example, we want to pass 143 not 14, 43, 1, 3, and 4.
                             if [[ $structure_positions =~ " $idx " && $brief -eq 0 ]]; then # same as above.
-                                echo "[ERROR] File name: $curr_file, $idx line, structure needs @brief tag "
+                                echo "[ERROR] File name: $curr_file, $idx line, structure needs @brief tag " >> $report_path
                                 check_result="failure"
                                 global_check_result="failure"
                                 latest_failed_file=$curr_file
@@ -143,7 +145,7 @@ function pr-prebuild-doxygen-tag(){
                             # Note that doxygen does not recognize a comment  statement that start with '/*'.
                             # Let's skip the doxygen tag inspection such as "/**" in case of a single line comment.
                             if [[ $line =~ "/*" && $line != *"/**"*  && ( $line != *"*/"  || $line =~ "@" ) && ( $idx != 1 ) ]]; then
-                                echo "[ERROR] File name: $curr_file, $idx line, Doxygen or multi line comments should begin with /**"
+                                echo "[ERROR] File name: $curr_file, $idx line, Doxygen or multi line comments should begin with /**" >> $report_path
                                 check_result="failure"
                                 global_check_result="failure"
                                 latest_failed_file=$curr_file
@@ -151,7 +153,7 @@ function pr-prebuild-doxygen-tag(){
 
                             # Check the doxygen tag written in upper case beacuase doxygen cannot use upper case tag such as '@TODO'.
                             if [[ $line =~ "@"[A-Z] ]]; then
-                                echo "[ERROR] File name: $curr_file, $idx line, The doxygen tag sholud be written in lower case."
+                                echo "[ERROR] File name: $curr_file, $idx line, The doxygen tag sholud be written in lower case." >> $report_path
                                 check_result="failure"
                                 global_check_result="failure"
                                 latest_failed_file=$curr_file
@@ -162,7 +164,7 @@ function pr-prebuild-doxygen-tag(){
                     ;;
                 # In case of Python code
                 *.py )
-                    echo "[DEBUG] ( $curr_file ) file is source code with the text format."
+                    echo "[DEBUG] ( $curr_file ) file is source code with the text format." >> $report_path
                     doxygen_lang="doxygen-python"
                     # Append a Doxgen rule step by step
                     doxygen_rules="@package @brief"
@@ -177,23 +179,23 @@ function pr-prebuild-doxygen-tag(){
 
                     for word in $doxygen_rules
                     do
-                        # echo "[DEBUG] $doxygen_lang: doxygen tag for current $doxygen_lang code is $word."
+                        # echo "[DEBUG] $doxygen_lang: doxygen tag for current $doxygen_lang code is $word." >> $report_path
                         doxygen_rule_all=$(( doxygen_rule_all + 1 ))
                         doxygen_rule[$doxygen_rule_all]=`cat ${curr_file} | grep "$word" | wc -l`
                         doxygen_rule_num=$(( $doxygen_rule_num + ${doxygen_rule[$doxygen_rule_all]} ))
                     done
                     if  [[ $doxygen_rule_num -le 0 ]]; then
-                        echo "[ERROR] $doxygen_lang: failed. file name: $curr_file, ($doxygen_rule_num)/$doxygen_rule_all tags are found."
+                        echo "[ERROR] $doxygen_lang: failed. file name: $curr_file, ($doxygen_rule_num)/$doxygen_rule_all tags are found." >> $report_path
                         check_result="failure"
                         global_check_result="failure"
                         latest_failed_file=$curr_file
                         break
                     else
-                        echo "[DEBUG] $doxygen_lang: passed. file name: $curr_file, ($doxygen_rule_num)/$doxygen_rule_all tags are found."
+                        echo "[DEBUG] $doxygen_lang: passed. file name: $curr_file, ($doxygen_rule_num)/$doxygen_rule_all tags are found." >> $report_path
                     fi
                     ;;
                 * )
-                    echo "[DEBUG] ( $curr_file ) file is not source code with the text format."
+                    echo "[DEBUG] ( $curr_file ) file is not source code with the text format." >> $report_path
                     ;;
             esac
         fi
